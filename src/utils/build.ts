d5e6741a7c071d576cb214b46a6e39ec3ffbc0c6
Bogdan <razvant.153957@e.unavarra.es>
2025-07-22 09:10:48 +0200

    Added formatted login to fastify server

--- src/utils/build.ts
+++ src/utils/build.ts
@@ -4,49 +4,64 @@ import swaggerUI from "@fastify/swagger-ui";
 
 // import top10 from "../endpoints/top10";
 
-export function build(opts = {}): FastifyInstance {
-    const app = Fastify(opts);
-    registerSwagger(app);
-    registerSwaggerUI(app);
-    registerRoutes(app);
-    return app;
+export function build(): FastifyInstance {
+  const app = Fastify({
+    logger:
+      process.env.NODE_ENV !== "test"
+        ? {
+            transport: {
+              target: "pino-pretty",
+              options: {
+                colorize: true,
+                translateTime: "SYS:standard",
+                ignore: "pid,hostname",
+              },
+            },
+          }
+        : false,
+  });
+
+  registerSwagger(app);
+  registerSwaggerUI(app);
+  registerRoutes(app);
+  return app;
 }
 
-function registerSwagger(app: FastifyInstance){
-    app.register(swagger, {
-      swagger: {
-        info: {
-          title: 'SeQura Backend',
-          version: '1.0.0',
-        },
+function registerSwagger(app: FastifyInstance) {
+  app.register(swagger, {
+    swagger: {
+      info: {
+        title: "SeQura Backend",
+        version: "1.0.0",
       },
-    });
+    },
+  });
+}
+
+function registerSwaggerUI(app: FastifyInstance) {
+  app.register(swaggerUI, {
+    routePrefix: "/docs",
+    uiConfig: {
+      docExpansion: "full",
+      deepLinking: false,
+    },
+    staticCSP: true,
+    transformSpecification: (swaggerObject, request, reply) => {
+      return swaggerObject;
+    },
+    transformSpecificationClone: true,
+  });
+}
+
+function registerRoutes(app: FastifyInstance) {
+  //app.register(top10);
+}
+export const start = async (fastify: FastifyInstance, PORT: number) => {
+  try {
+    await fastify.listen({ port: PORT, host: "0.0.0.0" });
+    console.log(`Server is running on http://localhost:${PORT}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
   }
-  
-  function registerSwaggerUI(app: FastifyInstance){
-    app.register(swaggerUI, {
-      routePrefix: '/docs',
-      uiConfig: {
-        docExpansion: 'full',
-        deepLinking: false
-      },
-      staticCSP: true,
-      transformSpecification: (swaggerObject, request, reply) => {
-        return swaggerObject;
-      },
-      transformSpecificationClone: true
-    });
-  }
-  
-  function registerRoutes(app: FastifyInstance){
-    //app.register(top10);
-  }
-  export   const start = async (fastify: FastifyInstance, PORT: number) => {
-    try {
-      await fastify.listen({ port: PORT, host: '0.0.0.0' });
-      console.log(`Server is running on http://localhost:${PORT}`);
-    } catch (err) {
-      fastify.log.error(err);
-      process.exit(1);
-    }
-  };
+};
