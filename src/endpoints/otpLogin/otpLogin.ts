dde08b7bbd9f9dffc641a2a10c50322906f691ff
Bogdan Razvant <bogdan21razvant@gmail.com>
2025-07-23 13:03:01 +0200

    [#18] - Cambiados los parametros nin y phone por un hash de sesion
    Co-authored-by: Bogdan <razvant.153957@e.unavarra.es>

--- src/endpoints/otpLogin/otpLogin.ts
+++ src/endpoints/otpLogin/otpLogin.ts
@@ -1,68 +1,47 @@
 import { FastifyInstance } from 'fastify';
 import { otpLoginSchema } from './schema';
-import { isValidNin } from '../../domain/helpers/validators/ninValidator';
-import { isValidPhone } from '../../domain/helpers/validators/phoneValidator';
 import { User } from '../../domain/userType';
 
 const OTP_LOGIN_ENDPOINT = '/login/otp';
 const MESSAGES = {
-  MISSING_CODE_NIN_OR_PHONE: 'Missing verification code, nin or phone number.',
-  INVALID_NIN_OR_PHONE: 'Invalid nin or phone number.',
-  INVALID_CODE: 'Invalid verification code.',
+  MISSING_HASH_OR_CODE: 'Missing hash or verification code.',
+  INVALID_HASH_OR_CODE: 'Invalid hash or verification code.',
+  INCORRECT_HASH_OR_CODE: 'Incorrect hash or verification code.',
   SUCCESSFULL_RESULT: 'User logged in successfully',
-  USER_BLOCKED: 'User is blocked.',
-  USER_NOT_FOUND: 'User not found.',
 };
 
-const BLOCKED_PHONE = '666666667';
-const BLOCKED_NIN = '12345678B';
-
-const VALID_NIN = '12345678A';
-const VALID_PHONE = '666666666';
+const VALID_HASH = '1234567890';
 const VALID_CODE = '123456';
 
 async function otpLogin(fastify: FastifyInstance) {
   fastify.post(OTP_LOGIN_ENDPOINT, otpLoginSchema, async (request, reply) => {
-    const { nin, phone, verificationCode } = request.body as User & { verificationCode: string };
+    const { hash, verificationCode } = request.body as User & {
+      hash: string;
+      verificationCode: string;
+    };
 
-    if (missingParameters(nin, phone, verificationCode)) {
-      return reply.status(400).send({ error: MESSAGES.MISSING_CODE_NIN_OR_PHONE });
+    if (missingParameters(hash, verificationCode)) {
+      return reply.status(400).send({ error: MESSAGES.MISSING_HASH_OR_CODE });
     }
 
-    if (invalidNinOrPhoneParameters(nin, phone)) {
-      return reply.status(400).send({ error: MESSAGES.INVALID_NIN_OR_PHONE });
+    if (invalidParameters(hash, verificationCode)) {
+      return reply.status(400).send({ error: MESSAGES.INVALID_HASH_OR_CODE });
     }
 
-    if (invalidCode(verificationCode)) {
-      return reply.status(400).send({ error: MESSAGES.INVALID_CODE });
-    }
-
-    if (blockedUser({ nin, phone })) {
-      return reply.status(403).send({ error: MESSAGES.USER_BLOCKED });
-    }
-
-    if (validUser({ nin, phone })) {
-      return reply.status(200).send({ message: MESSAGES.SUCCESSFULL_RESULT });
-    }
-
-    return reply.status(404).send({ error: MESSAGES.USER_NOT_FOUND });
+    return reply.status(200).send({ message: MESSAGES.SUCCESSFULL_RESULT });
   });
 }
 
-function missingParameters(nin: string, phone: string, verificationCode: string): boolean {
-  return !nin || !phone || !verificationCode;
+function missingParameters(hash: string, verificationCode: string): boolean {
+  return !hash || !verificationCode;
 }
 
-function invalidNinOrPhoneParameters(nin: string, phone: string): boolean {
-  return !isValidNin(nin) || !isValidPhone(phone);
+function invalidParameters(hash: string, verificationCode: string): boolean {
+  return invalidHash(hash) || invalidCode(verificationCode);
 }
 
-function blockedUser(user: User): boolean {
-  return user.nin === BLOCKED_NIN && user.phone === BLOCKED_PHONE;
-}
-
-function validUser(user: User): boolean {
-  return user.nin === VALID_NIN && user.phone === VALID_PHONE;
+function invalidHash(hash: string): boolean {
+  return hash !== VALID_HASH;
 }
 
 function invalidCode(verificationCode: string): boolean {
