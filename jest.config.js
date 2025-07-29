574a2010fee02f5f6bfa2f44329ba2ac38208805
Beñat <150382660+Benat-Iribarren@users.noreply.github.com>
2025-07-29 12:25:40 +0200

    Preparar el entorno de testing (#90)

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const { createDefaultPreset } = require('ts-jest');
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import('jest').Config} **/
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  clearMocks: true,
+};
