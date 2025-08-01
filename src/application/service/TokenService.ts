2747693849d6773ec116b2d5b7d3e164451956de
Beñat <150382660+Benat-Iribarren@users.noreply.github.com>
2025-08-01 10:34:29 +0200

    Eliminar tokenStorage para adecuarse a la arquitectura hexagonal (#135)

--- src/application/service/TokenService.ts
+++ src/application/service/TokenService.ts
@@ -2,11 +2,11 @@ import { Token } from '../../domain/model/tokenType';
 import { Hash } from '../../domain/model/hashType';
 import { generateTokenGivenHash } from '../../infrastructure/helpers/tokenGenerator';
 import { TokenService } from '../../domain/interfaces/tokenService';
-import { tokenStorage } from '../../infrastructure/tokenStorage';
+import { tokenRepository } from '../../infrastructure/database/operations/tokenOperations';
 
 export const TokenServiceImpl: TokenService = {
   async saveToken(token) {
-    return tokenStorage.saveToken(token);
+    return tokenRepository.saveTokenToDb(token);
   },
   generateToken(hash: Hash): Token {
     return generateTokenGivenHash(hash);
--- src/domain/interfaces/tokenStorage.ts
+++
@@ -1,5 +0,0 @@
-import { Token } from '../model/tokenType';
-
-export interface TokenStorage {
-  saveToken(token: Token): Promise<void>;
-}
