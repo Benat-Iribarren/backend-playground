26985027029756af45032bc2a20568a9d739aa71
Beñat <150382660+Benat-Iribarren@users.noreply.github.com>
2025-08-04 13:35:45 +0200

    Anadir esquemas de request a los endpoints (#161)

--- src/infrastructure/endpoints/auth/request-otp/schema.ts
+++ src/infrastructure/endpoints/auth/request-otp/schema.ts
@@ -2,6 +2,14 @@ import { errorSchema } from '../../common/errorSchema';
 
 export const requestOtpSchema = {
   schema: {
+    body: {
+      type: 'object',
+      properties: {
+        nin: { type: 'string' },
+        phone: { type: 'string' },
+      },
+      additionalProperties: false,
+    },
     response: {
       200: {
         type: 'object',
--- src/infrastructure/endpoints/auth/verify-otp/schema.ts
+++ src/infrastructure/endpoints/auth/verify-otp/schema.ts
@@ -2,6 +2,14 @@ import { errorSchema } from '../../common/errorSchema';
 
 export const verifyOtpSchema = {
   schema: {
+    body: {
+      type: 'object',
+      properties: {
+        hash: { type: 'string' },
+        verificationCode: { type: 'string' },
+      },
+      additionalProperties: false,
+    },
     response: {
       200: {
         type: 'object',
