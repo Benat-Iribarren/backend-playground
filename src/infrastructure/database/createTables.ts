b46fe1a3e412e90fec599568df0d09d5a9fd4b6a
Beñat <150382660+Benat-Iribarren@users.noreply.github.com>
2025-08-07 12:21:02 +0200

    Modificar userId de las tablas de Otp y token para que sean FK y token y hash para que sean los PK (#210)

--- src/infrastructure/database/createTables.ts
+++ src/infrastructure/database/createTables.ts
@@ -2,21 +2,24 @@ import { db } from './createDatabaseFile';
 
 export const createTables = async () => {
   db.exec(
-    `CREATE TABLE IF NOT EXISTS otp (
-        userId TEXT PRIMARY KEY,
-        hash TEXT NOT NULL,
-        verificationCode TEXT NOT NULL,
-        expirationDate TEXT NOT NULL
-    );
-    CREATE TABLE IF NOT EXISTS token (
-        userId NUMBER PRIMARY KEY,
-        token TEXT NOT NULL
-    );
+    `
     CREATE TABLE IF NOT EXISTS user (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         nin TEXT NOT NULL,
         phone TEXT NOT NULL,
         isBlocked BOOLEAN NOT NULL
+    );
+    CREATE TABLE IF NOT EXISTS otp (
+        hash TEXT PRIMARY KEY,
+        userId INTEGER NOT NULL,
+        verificationCode TEXT NOT NULL,
+        expirationDate TEXT NOT NULL,
+        FOREIGN KEY (userId) REFERENCES user(id)
+    );
+    CREATE TABLE IF NOT EXISTS token (
+        token TEXT PRIMARY KEY,  
+        userId INTEGER NOT NULL,
+        FOREIGN KEY (userId) REFERENCES user(id)
     );
     `,
   );
