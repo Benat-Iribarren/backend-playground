b2c9db03465e6344009d9f365b741cc171d7fffe
Beñat <150382660+Benat-Iribarren@users.noreply.github.com>
2025-08-25 09:02:54 +0200

    [TEST] - Implementar test E2E para updateProfile (#438)

---
+++ src/user/infrastructure/endpoints/updateProfile/__test__/e2e/updateProfile.test.ts
@@ -0,0 +1,51 @@
+import { FastifyInstance } from 'fastify';
+import { build } from '@common/infrastructure/server/serverBuild';
+import { initTestDatabase } from '@common/infrastructure/database/initTestDatabase';
+import { UPDATE_PROFILE_ENDPOINT } from '../../updateProfile';
+import { REQUEST_OTP_ENDPOINT } from '@auth/infrastructure/endpoints/requestOtp/requestOtp';
+import { VERIFY_OTP_ENDPOINT } from '@auth/infrastructure/endpoints/verifyOtp/verifyOtp';
+
+describe('updateProfile e2e', () => {
+  let app: FastifyInstance;
+
+  beforeAll(async () => {
+    app = build();
+    await app.ready();
+    await initTestDatabase();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  test('should return success message for valid user (happy path)', async () => {
+    const nin = '87654321Z';
+    const phone = '222222222';
+
+    const requestOtpRes = await app.inject({
+      method: 'POST',
+      url: REQUEST_OTP_ENDPOINT,
+      payload: { nin, phone },
+    });
+    expect(requestOtpRes.statusCode).toBe(201);
+    const { hash, verificationCode } = requestOtpRes.json();
+
+    const verifyOtpRes = await app.inject({
+      method: 'POST',
+      url: VERIFY_OTP_ENDPOINT,
+      payload: { hash, verificationCode },
+    });
+    expect(verifyOtpRes.statusCode).toBe(201);
+    const { token } = verifyOtpRes.json();
+
+    const profileRes = await app.inject({
+      method: 'PUT',
+      url: UPDATE_PROFILE_ENDPOINT,
+      headers: { authorization: `Bearer ${token}` },
+      payload: { fullName: 'New Name' },
+    });
+
+    expect(profileRes.statusCode).toBe(200);
+    expect(profileRes.json()).toEqual({ message: 'Profile updated successfully.' });
+  });
+});
