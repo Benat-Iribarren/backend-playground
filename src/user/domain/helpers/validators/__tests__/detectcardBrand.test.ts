40b7f8ba89b50735c7dfbf799824d0a7b60b0354
Beñat <150382660+Benat-Iribarren@users.noreply.github.com>
2025-08-26 11:49:17 +0200

    [TEST] - Implementar test unitarios para detectCardBrand (#476)

---
+++ src/user/domain/helpers/validators/__tests__/detectcardBrand.test.ts
@@ -0,0 +1,19 @@
+import { detectCardBrand } from '@user/domain/helpers/validators/detectCardBrand';
+
+describe('detectCardBrand', () => {
+  it('returns VISA', () => {
+    expect(detectCardBrand('4111111111111111')).toBe('VISA');
+  });
+
+  it('returns MASTERCARD', () => {
+    expect(detectCardBrand('5555555555554444')).toBe('MASTERCARD');
+  });
+
+  it('returns AMEX', () => {
+    expect(detectCardBrand('371449635398431')).toBe('AMEX');
+  });
+
+  it('returns UNKNOWN for others', () => {
+    expect(detectCardBrand('1234567890123456')).toBe('UNKNOWN');
+  });
+});
