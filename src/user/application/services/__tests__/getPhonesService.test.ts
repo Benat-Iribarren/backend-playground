9e39b0f1f29f0d3dffd417bc3dbc315377aaadb2
Beñat <150382660+Benat-Iribarren@users.noreply.github.com>
2025-08-29 08:39:53 +0200

    [TEST] - Implementar tests de servicio (#557)

---
+++ src/user/application/services/__tests__/getPhonesService.test.ts
@@ -0,0 +1,66 @@
+import { processGetPhones } from '../getPhonesService';
+import { UserRepository } from '@user/domain/interfaces/repositories/UserRespository';
+
+function createMockRepo(): jest.Mocked<UserRepository> {
+  return {
+    getUser: jest.fn(),
+    getProfile: jest.fn(),
+    updateProfile: jest.fn(),
+    isUserPhoneRegistered: jest.fn(),
+    getPhones: jest.fn(),
+  };
+}
+
+describe('getPhonesService', () => {
+  beforeEach(() => jest.resetAllMocks());
+
+  test('should return user phone info list for given an userId with added phones', async () => {
+    const userId = 1;
+    const repo = createMockRepo();
+
+    repo.getPhones.mockResolvedValue([
+      {
+        phoneNumber: '1234567890',
+        isPrimary: true,
+      },
+      {
+        phoneNumber: '0987654321',
+        isPrimary: false,
+      },
+    ]);
+
+    const result = await processGetPhones(repo, { userId });
+
+    expect(repo.getPhones).toHaveBeenCalledWith(1);
+    expect(result).toEqual({
+      phones: [
+        { phoneNumber: '1234567890', isPrimary: true },
+        { phoneNumber: '0987654321', isPrimary: false },
+      ],
+    });
+  });
+
+  test('should return user phone info list for given an userId without added phones', async () => {
+    const userId = 1;
+    const repo = createMockRepo();
+
+    repo.getPhones.mockResolvedValue(null);
+
+    const result = await processGetPhones(repo, { userId });
+
+    expect(repo.getPhones).toHaveBeenCalledWith(1);
+    expect(result).toEqual({
+      phones: [],
+    });
+  });
+
+  test('should return empty array when repo returns null', async () => {
+    const userId = 1;
+    const repo = createMockRepo();
+    repo.getPhones.mockResolvedValue(null);
+
+    const result = await processGetPhones(repo, { userId });
+
+    expect(result).toEqual({ phones: [] });
+  });
+});
